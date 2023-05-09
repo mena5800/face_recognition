95fb41aa9ce867a725db373796aee00bbbba1946
mina safwat <mr.safwat55@gmail.com>
2023-05-09 15:31:10 +0300

    add model.pkl

--- convert_to_matrix.py
+++ convert_to_matrix.py
@@ -21,12 +21,16 @@ def convert_to_mat(dir_path,images,label):
 
 images = [[],[]]
 
-convert_to_mat("mina_george_pre_images",images,0)
-convert_to_mat("mina_prepocessing_images",images,1)
-convert_to_mat("abram_pre",images,2)
+convert_to_mat("images/pre_processing/mina",images,0)
+convert_to_mat("images/pre_processing/abram",images,1)
+convert_to_mat("images/pre_processing/mahmoud",images,2)
+convert_to_mat("images/pre_processing/mariam_h",images,3)
+convert_to_mat("images/pre_processing/mariam_m",images,4)
+
+images.append({0:"mina",1:"abram",2:"mahmoud",3:"mariam hossam",4:"mariam mohamed"})
 
 
 
 # Save the list to a file using pickle
-with open("output_file.pkl", "wb") as f:
+with open("dataset.pkl", "wb") as f:
     pickle.dump(images, f)
--- preprocessing_images.py
+++ preprocessing_images.py
@@ -1,7 +1,8 @@
 import os
 import cv2
 
-def preimage(dir_path,dir_path_save):
+
+def preimage(dir_path, dir_path_save):
 
     # Load the pre-trained face detection classifier
     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
@@ -20,20 +21,25 @@ def preimage(dir_path,dir_path_save):
             resized_img = cv2.resize(img, (new_width, new_height))
             gray = cv2.cvtColor(resized_img, cv2.COLOR_BGR2GRAY)
 
-            faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+            faces = face_cascade.detectMultiScale(
+                gray, scaleFactor=1.1, minNeighbors=5)
 
             for (x, y, w, h) in faces:
                 cropped_img = gray[y:y+h, x:x+w]
 
-            cropped_img = cv2.resize(cropped_img,(50,37))
-
-            # Save the resized image with a new filename
-            new_filename = os.path.splitext(filename)[0] + "_resized.jpg"
-            cv2.imwrite(os.path.join(dir_path_save, new_filename), cropped_img)
-
-
-
-
-preimage("abram","abram_pre")
+            try:
+                cropped_img = cv2.resize(cropped_img, (50, 37))
+                # Save the resized image with a new filename
+                new_filename = os.path.splitext(filename)[0] + "_resized.jpg"
+                cv2.imwrite(os.path.join(dir_path_save, new_filename), cropped_img)
+            except:
+                pass
 
 
+
+
+preimage("images/post_processing/abram", "images/pre_processing/abram")
+preimage("images/post_processing/mahmoud", "images/pre_processing/mahmoud")
+preimage("images/post_processing/mariam_m", "images/pre_processing/mariam_m")
+preimage("images/post_processing/mina", "images/pre_processing/mina")
+preimage("images/post_processing/mariam_h", "images/pre_processing/mariam_h")
