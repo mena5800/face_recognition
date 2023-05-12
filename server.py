12396aa7ba47f5f0e6db2624f13d1047161f3f6b
Abram1111 <ebram.meawad01@eng-st.cu.edu.eg>
2023-05-12 13:49:34 +0300

    css

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import pickle
 from flask import Flask, render_template, request
 import io
 from PIL import Image
@@ -7,30 +8,28 @@ from face_recognition import predict_image
 
 app = Flask(__name__, static_url_path='/static')
 
-import pickle
 
 # Load the saved model from the file
-with open('trained_model.pkl', 'rb') as f:
-    pca, clf ,names= pickle.load(f)
-
+with open('D:/git/face_recognition/trained_models.pkl', 'rb') as f:
+    pca, clf, names = pickle.load(f)
 
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+
 @app.route('/transform', methods=['POST'])
 def transform():
-# Get the uploaded image
+    # Get the uploaded image
     image_file = request.files['image-file']
 
-    
     # Load the image using PIL
     image = Image.open(image_file)
     # convert the pil object to numpy array
     image = np.array(image)
 
-    prediction = predict_image(image,clf,pca,names)
+    prediction = predict_image(image, clf, pca, names)
 
     # use face_detection function to return image with face detection
     image = face_detection(image)
@@ -42,8 +41,7 @@ def transform():
     image.save(output_buffer, format='JPEG')
 
     # Return the output image
-    return output_buffer.getvalue(),prediction
-
+    return output_buffer.getvalue(), prediction
 
 
 if __name__ == '__main__':
--- static/script.js
+++ static/script.js
@@ -4,34 +4,35 @@ const inputImageButton = document.getElementById('input-image-button');
 const inputImagecontainer = document.getElementById('input-container');
 const inputImage = document.getElementById('input-image');
 const prediction = document.getElementById('prediction');
+const custom_upload_btn = document.getElementById('custom_btn')
 
 
 
 
 inputImageButton.addEventListener('change', async (event) => {
 
-    // Create a new FileReader object
-    var reader = new FileReader();
+  // Create a new FileReader object
+  var reader = new FileReader();
 
-    // Listen for the load event of the FileReader
-    reader.addEventListener('load', function() {
+  // Listen for the load event of the FileReader
+  reader.addEventListener('load', function () {
 
-        inputImage.src = reader.result;
+    inputImage.src = reader.result;
 
-    });
-  
-    // Read the selected file as a data URL
-    reader.readAsDataURL(inputImageButton.files[0]);
+  });
+
+  // Read the selected file as a data URL
+  reader.readAsDataURL(inputImageButton.files[0]);
   event.preventDefault();
-  
+
   const formData = new FormData(form);
-  
+
   try {
     const response = await fetch('/transform', {
       method: 'POST',
       body: formData
     });
-    
+
     const blob = await response.blob();
     const url = URL.createObjectURL(blob);
     prediction.innerText = response.statusText
@@ -43,3 +44,5 @@ inputImageButton.addEventListener('change', async (event) => {
 });
 
 
+
+custom_upload_btn.addEventListener('click', function () { inputImageButton.click(); })
